cd6c3917a689ca03303998721eb2c8dd5b0fd5b9
elizabeth zhang <elizabeth.zhangey@gmail.com>
2024-08-06 10:28:23 -0400

    refactor: remove unnecessary styles import from next root layout

--- demos/next-js/src/app/package-import/layout.tsx
+++ demos/next-js/src/app/package-import/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "@newjersey/njwds-components/packages/stencil-library/dist/stencil-library/css/styles.css";
 
 export const metadata: Metadata = {
     title: "Next demo - package import example",
